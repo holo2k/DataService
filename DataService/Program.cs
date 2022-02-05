1f6fcd98d44665d7338ca5612f44b0424e47f78d
holo2k <nneketaa@yandex.ru>
2022-02-05 18:21:23 +0500

    TemperatureUpdate(Error)

--- DataService/Program.cs
+++ DataService/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ServiceProcess;
 
 namespace DataService
 {
@@ -12,7 +7,7 @@ namespace DataService
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
-        static void Main()
+        public static void Main()
         {
             ServiceBase[] ServicesToRun;
             ServicesToRun = new ServiceBase[]
